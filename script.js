831f94bb6b55c95552ef1596a7cf2f645a8bc7be
Wilmar <wilmarlofranco@gmail.com>
2024-08-27 19:22:31 +0800

    Add all operations and clear function

--- script.js
+++ script.js
@@ -1,10 +1,87 @@
-// Operations
 
-function operate (a, b) {
-    return a ${operator} b;
+let a = "";
+let b = "";
+let operator = "";
+let displayValue = "";
+
+let display = document.querySelector("#display");
+let numbers = document.querySelectorAll("[id^='number']");
+let operators = document.querySelectorAll("[id^='operator']");
+let equals = document.querySelector("#equals");
+let clear = document.querySelector("#clear");
+
+ function handleNumberClick(event) {
+    if (operator === "") {
+        if (a.length < 16) {
+        a += event.target.value;
+        displayValue = a;
+        display.textContent = displayValue;
+        }
+    } else {
+        if (b.length < 16) {
+        b += event.target.value;
+        displayValue = b;
+        display.textContent = displayValue;
+        }
+    }
+    if (a.length === 16 && b.length === 16) {
+        numbers.forEach(function (button) {
+            button.removeEventListener('click', handleNumberClick)
+        })
+    }
 }
 
-let a = 0;
-let b = 0;
-let operators = "";
+ function handleOperatorClick(event) {
+    operator = event.target.value;
+}
 
+function operate () {
+    let result;
+    let numA = parseFloat(a);
+    let numB = parseFloat(b);
+    
+    switch (operator) {
+        case '+':
+            result = numA + numB;
+            break;
+        case '-':
+            result = numA - numB;
+            break;
+        case '*':
+            result = numA * numB;
+            break;
+        case '/':
+            if (numA === 0 || numB ===0) {
+                result = "Error";
+            } else {
+            result = numA / numB;
+            }
+            break;
+        default:
+            result = "Error";
+}
+    displayValue = result;
+    display.textContent = displayValue;
+    a = result.toString();
+    b = "",
+    operator = "";
+}
+
+function erase () {
+    a = "";
+    b = "";
+    displayValue = "";
+    display.textContent = "";
+    operator = ""; 
+}
+
+numbers.forEach(function(button) {
+    button.addEventListener('click', handleNumberClick);
+})
+
+operators.forEach(function(button) {
+    button.addEventListener('click', handleOperatorClick);
+})
+
+equals.addEventListener('click', operate);
+clear.addEventListener('click', erase);
